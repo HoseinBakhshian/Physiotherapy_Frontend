52c8d3710d8c54b0b1697839e00c6bb3348d2bd0
HoseinBakhshian <98746693+HoseinBakhshian@users.noreply.github.com>
2024-09-04 01:25:54 +0330

    Update MainAxios.js

--- src/services/MainAxios.js
+++ src/services/MainAxios.js
@@ -3,6 +3,10 @@ import axios from "axios";
 export const MainAxios= axios.create({
     baseURL: "https://physiotherapy-backend.onrender.com",
     withCredentials: true,
-    timeoutErrorMessage: "error az samte server"
+    timeoutErrorMessage: "error az samte server",
+    headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true,
+      }
 
 })
